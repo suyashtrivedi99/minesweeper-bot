7d506728d51c844a1d74f223b8dadd2e87f04612
Suyash Trivedi <suyash.trivedi99@gmail.com>
2020-08-30 00:12:49 +0530

    Added utility function for scanning all tiles

--- capture.py
+++ capture.py
@@ -1,56 +1,51 @@
+import os  
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import pyautogui as pg
-import time
-import numpy as np
-import cv2
-import secrets 
-import string 
 from PIL import Image
+from tensorflow.keras.models import load_model
+import numpy as np
 
-#size of each tile
-bsize = 52.5
-
-#name length of image files
-fnamelen = 50
-
-#no of rows and columns, and tiles
-n = 9
-tiles = n * n
-
-first = pg.locateCenterOnScreen('tab.png')
-
-#coordinates of the grid
-sx = first.x - (bsize/2)
-sy = first.y - (bsize/2)
-ex = first.x + ((n - 0.5)*bsize)
-ey = first.y + ((n - 0.5)*bsize)
-
-#wait for 120 secs to click all 4 corners    
-time.sleep(1)
-
-#take a screen shot of the grid
-img = pg.screenshot(region=(sx,sy,ex-sx,ey-sy))
-img = np.asarray(img)
+def scan(n, sx, sy, ex, ey, off):
+    tiles = []
     
-ih = img.shape[0]
-iw = img.shape[1]
-ih = ih // n
-iw = iw // n
-
-for i in range(n):
-    for j in range(n):
-        off = 8    
-        #select the current tile from the whole image
-        cur = img[i*ih + off:(i+1)*ih - off,j*iw + off:(j+1)*iw - off]
-       
-        #path to save tile image
-        res = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(fnamelen)) 
-        path = r"./img/" + str(res) + ".png"
+    #loading the model
+    model = load_model('model.h5')
+    
+    #take a screen shot of the grid
+    img = pg.screenshot(region=(sx,sy,ex-sx,ey-sy))
+    img = np.asarray(img)
         
-        #convert this snap of a tile into an image and save it
-        cur = Image.fromarray(cur).convert('RGB')
-        cur.save(path)
+    ih = img.shape[0]
+    iw = img.shape[1]
+    ih = ih // n
+    iw = iw // n
+    
+    for i in range(n):
+        curtiles = []
         
-        #cv2.imshow("frame",cur)
-        #cv2.waitKey(0)
+        for j in range(n):
+            #off = 8    
+            #select the current tile from the whole image
+            cur = img[i*ih + off:(i+1)*ih - off,j*iw + off:(j+1)*iw - off]
+            
+            """
+            #path to save tile image
+            res = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(fnamelen)) 
+            path = r"./img/" + str(res) + ".png"
+            """
+            
+            #convert this snap of a tile into an image and save it
+            cur = Image.fromarray(cur).convert('RGB')
+            cur = np.asarray(cur)
+            
+            #scanning the tile image by using the CNN model
+            predArr = model.predict(np.expand_dims(cur, axis=0))[0]
+            num = np.where(predArr == np.amax(predArr))
+            
+            #adding to the current row of tiles
+            curtiles.append(num[0][0])
+        
+        tiles.append(curtiles)
 
-cv2.destroyAllWindows()       
+    return tiles
+        
