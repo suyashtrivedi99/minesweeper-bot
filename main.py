3f6f51e62d335bdbb7e49ab06c0ed33df6370c62
Suyash Trivedi <suyash.trivedi99@gmail.com>
2020-08-31 14:12:12 +0530

    Update GUI Automation and tab.PNG

--- main.py
+++ main.py
@@ -41,14 +41,16 @@ class grid:
         self.h = endy - starty
 
 mat = grid()
-temp = []
 
 def initialize():
     global buttons
     global mat
-    global temp
+    global n
     
     first = pg.locateCenterOnScreen('tab.png')
+    
+    #click on first tile to start
+    pg.click(first)
     
     #coordinates of the grid
     gstartx = first.x - (bsize/2)
@@ -60,10 +62,12 @@ def initialize():
     mat = grid(n,n,gstartx,gstarty,gendx,gendy)    
     
     for i in range(n):
+        curbuttons = []
         for j in range(n):
-            b = button(first.x + (j*bsize),first.y + (i*bsize),0)
-            buttons.append(b)
-            
+            b = button(first.x + (j*bsize),first.y + (i*bsize),9)
+            curbuttons.append(b)
+        buttons.append(curbuttons)
+    """    
     for b in buttons:
         pg.click(b.x,b.y)
         
@@ -71,9 +75,149 @@ def initialize():
             break
    
     temp = scan(n, mat.startx, mat.starty, mat.endx, mat.endy, 8)
-             
+    """
+
+#update the status of all tiles
+def update_tiles():
+    global buttons
+    global mat
+    global n
+    
+    temp_list = scan(n, mat.startx, mat.starty, mat.endx, mat.endy, 8)
+    
+    for i in range(n):
+        for j in range(n):
+           buttons[i][j].status = temp_list[i][j]
+
+#check if end has been reached
+def check_run():
+    global buttons
+    global mat
+    
+    for button in buttons:
+        #bomb detection
+        if button.status == 11:
+            return False
+    return True     
+
+#solve
+def solve():
+    global buttons
+    global mat
+    global n
+    
+    for i in range(n):
+        for j in range(n):
+            curs = buttons[i][j].status
+            
+            if 1 <= curs <= 8:
+                flag = []
+                risk = []
+                
+                #top-left tile
+                curi = i-1
+                curj = j-1
+                
+                if 0 <= curi <= n-1 and 0 <= curj <= n-1:
+                    if buttons[curi][curj].status == 9:
+                        risk.append((curi,curj))
+                    elif buttons[curi][curj].status == 10:
+                        flag.append((curi,curj))
+                
+                #top-middle tile
+                curi = i-1
+                curj = j
+                
+                if 0 <= curi <= n-1 and 0 <= curj <= n-1:
+                    if buttons[curi][curj].status == 9:
+                        risk.append((curi,curj))
+                    elif buttons[curi][curj].status == 10:
+                        flag.append((curi,curj))
+                
+                #top-right tile
+                curi = i-1
+                curj = j+1
+                
+                if 0 <= curi <= n-1 and 0 <= curj <= n-1:
+                    if buttons[curi][curj].status == 9:
+                        risk.append((curi,curj))
+                    elif buttons[curi][curj].status == 10:
+                        flag.append((curi,curj))
+    
+                #middle-left tile
+                curi = i
+                curj = j-1
+                
+                if 0 <= curi <= n-1 and 0 <= curj <= n-1:
+                    if buttons[curi][curj].status == 9:
+                        risk.append((curi,curj))
+                    elif buttons[curi][curj].status == 10:
+                        flag.append((curi,curj))
+                
+                #middle-right tile
+                curi = i
+                curj = j+1
+                
+                if 0 <= curi <= n-1 and 0 <= curj <= n-1:
+                    if buttons[curi][curj].status == 9:
+                        risk.append((curi,curj))
+                    elif buttons[curi][curj].status == 10:
+                        flag.append((curi,curj))
+
+                #bottom-left tile
+                curi = i+1
+                curj = j-1
+                
+                if 0 <= curi <= n-1 and 0 <= curj <= n-1:
+                    if buttons[curi][curj].status == 9:
+                        risk.append((curi,curj))
+                    elif buttons[curi][curj].status == 10:
+                        flag.append((curi,curj))
+                
+                #bottom-middle tile
+                curi = i+1
+                curj = j
+                
+                if 0 <= curi <= n-1 and 0 <= curj <= n-1:
+                    if buttons[curi][curj].status == 9:
+                        risk.append((curi,curj))
+                    elif buttons[curi][curj].status == 10:
+                        flag.append((curi,curj))
+                
+                #bottom-right tile
+                curi = i+1
+                curj = j+1
+                
+                if 0 <= curi <= n-1 and 0 <= curj <= n-1:
+                    if buttons[curi][curj].status == 9:
+                        risk.append((curi,curj))
+                    elif buttons[curi][curj].status == 10:
+                        flag.append((curi,curj))
+
+                #what action to take?
+                clickside = ""
+                
+                #if only the bombs remain unmarked
+                if curs - len(flag) == risk:
+                    clickside = "right"
+                    
+                #if all the bombs have already been marked    
+                elif curs == len(flag):
+                    clickside = "left"
+                
+                for coord in risk:
+                    clickcoord = (buttons[coord[0]][coord[1]].x, buttons[coord[0]][coord[1]].y)    
+                    pg.click(x=clickcoord[0],y=clickcoord[1],button=clickside)
+                        
+
+#start execution 
 initialize()
 
-#%%
-
-print(np.matrix(temp))
+while check_run():
+        
+    #update board
+    update_tiles()
+    
+    #solve the board
+    solve()
+                    
